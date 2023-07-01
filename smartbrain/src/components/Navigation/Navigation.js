4afecbc2194c977d549fe361a34ed4cfd3e4e5b7
mimminou <mimminou@github.com>
2023-07-01 17:14:16 +0100

    starting project

---
+++ smartbrain/src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav style={{display: "flex", justifyContent: "flex-end"}}>
+      <span className="f3 link dim black pa3 pointer">Sign Out</span>       
+    </nav>
+  )
+}
+
+export default Navigation;
