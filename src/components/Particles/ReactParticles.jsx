7e84e857f3c06fffb31c7c2c9877879b92f40afe
mimminou <mimminou@gmail.com>
2023-07-03 18:00:31 +0100

    cleaned up particles options

--- src/components/Particles/ReactParticles.jsx
+++ src/components/Particles/ReactParticles.jsx
@@ -1,6 +1,7 @@
 import { useCallback } from "react";
 import Particles from "react-particles";
 import { loadFull } from "tsparticles";
+import options from "./ParticleOptions";
 
 const ReactParticles = () => {
     const particlesInit = useCallback(async engine => {
@@ -10,81 +11,6 @@ const ReactParticles = () => {
     const particlesLoaded = useCallback(async container => {
         await console.log(container);
     }, []);
-
-    const options = {
-            background: {
-                color: {
-                    value: "#0d47a1",
-                },
-                opacity: 0,
-            },
-            fpsLimit: 60,
-            interactivity: {
-                events: {
-                    onClick: {
-                        enable: false,
-                        mode: "push",
-                    },
-                    onHover: {
-                        enable: true,
-                        mode: "repulse",
-                    },
-                    resize: true,
-                },
-                modes: {
-                    push: {
-                        quantity: 4,
-                    },
-                    repulse: {
-                        distance: 100,
-                        duration: 0.4,
-                    },
-                },
-            },
-            particles: {
-                color: {
-                    value: "#ffffff",
-                },
-                links: {
-                    color: "#ffffff",
-                    distance: 200,
-                    enable: true,
-                    opacity: 0.5,
-                    width: 1,
-                },
-                collisions: {
-                    enable: true,
-                },
-                move: {
-                    direction: "none",
-                    enable: true,
-                    outModes: {
-                        default: "bounce",
-                    },
-                    random: false,
-                    speed: 3,
-                    straight: false,
-                },
-                number: {
-                    density: {
-                        enable: true,
-                        area: 800,
-                    },
-                    value: 25,
-                },
-                opacity: {
-                    value: 0.5,
-                },
-                shape: {
-                    type: "circle",
-                },
-                size: {
-                    value: { min: 1, max: 5 },
-                },
-            },
-            detectRetina: true,
-        
-    }
 
     return (
         <Particles className="absolute z-[-1]" id="tsparticles" init={particlesInit} loaded={particlesLoaded} options={options} />
